9858c4d567ef32cd54ff722abd4960b652a102e6
niamurndc <niamurndc17@gmail.com>
2020-08-06 22:46:03 +0600

    file uploaded

---
+++ javascript/code.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:5,
+  nav:true,
+  dots: false,
+  responsive:{
+      0:{
+          items:2
+      },
+      600:{
+          items:4
+      },
+      1000:{
+          items:6
+      }
+  }
+})
